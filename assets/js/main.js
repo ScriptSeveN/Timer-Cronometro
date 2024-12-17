3227b2a5dc7bb6847b0349b82003c82f5449b0a6
Marco Almeida <130010754+ScriptSeveN@users.noreply.github.com>
2024-12-17 20:53:17 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,30 @@
-const startButton = document.querySelector('.iniciar');
-const stopButton = document.querySelector('.pausar');
-const clearButton = document.querySelector('.zerar');
-const timer = document.querySelector('.relogio');
-let seconds = 0;
-let timerStop = null;
+function clock() {
+    const timer = document.querySelector('.relogio');
+    let seconds = 0;
+    let timerStop = null;
 
-function getSecond(second) {
-    let data = new Date(second * 1000);
-    return data.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        second: '2-digit',
-        minute: '2-digit',
-        hour: '2-digit',
-        timeZone: 'UTC'
+    // Formata os segundos no formato HH:MM:SS
+    const formatTime = (sec) =>
+        new Date(sec * 1000).toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+
+    // Evento para manipular os botões
+    document.addEventListener('click', ({ target }) => {
+        if (target.classList.contains('iniciar') && !timerStop) {
+            timerStop = setInterval(() => (timer.innerHTML = formatTime(++seconds)), 1000);
+            timer.classList.remove('stopClock');
+        } else if (target.classList.contains('pausar')) {
+            clearInterval(timerStop);
+            timer.classList.add('stopClock');
+            timerStop = null;
+        } else if (target.classList.contains('zerar')) {
+            clearInterval(timerStop);
+            timerStop = seconds = null;
+            timer.innerHTML = formatTime(0);
+            timer.classList.remove('stopClock');
+        }
     });
 }
-
-function startTimer() {
-    if(timerStop === null) {
-        timerStop = setInterval(()=>{
-            let timerValue = getSecond(seconds);
-            timer.innerHTML = `${timerValue}`;
-            timer.classList.remove('stopClock');
-            seconds++
-        }, 1000);
-    }  
-}
-
-function stopTimer() {
-    clearInterval(timerStop);
-    timer.classList.add('stopClock');
-    timerStop = null
-}
-
-function clearTimer() {
-    timer.innerHTML = getSecond(0);
-    clearInterval(timerStop);
-    timer.classList.remove('stopClock');
-    seconds = 0;
-    timerStop = null;
-}
-
-startButton.addEventListener('click', startTimer);
-stopButton.addEventListener('click', stopTimer);
-clearButton.addEventListener('click', clearTimer);
+clock();
